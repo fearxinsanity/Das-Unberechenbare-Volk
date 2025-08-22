59cbeb41b3abe3d397fd1e91199669c4a1f1cd2c
Nico Hoffmann <dev.nhoffmann@proton.me>
2025-08-22 22:21:23 +0200

    Create SimulationEngine.java
    Implementierung der SimulationsEngine Logic

---
+++ src/model/SimulationEngine.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class SimulationEngine {
+    private List<Party> parties;
+    private List<Voter> voters;
+
+    public SimulationEngine(int numberOfVoters, int numberOfParties) {
+        this.parties = new ArrayList<>(numberOfParties);
+        this.voters = new ArrayList<>(numberOfVoters);
+
+        for (int i = 0; i < numberOfVoters; i++) {
+            parties.add(new Party("Party " + (i + 1), "Ideology " + (i + 1), 100000.0, 0));
+        }
+
+        Random rand = new Random();
+        for (int i = 0; i < numberOfVoters; i++) {
+            Party initialPreference = parties.get(rand.nextInt(numberOfParties));
+            double opinionStrength = rand.nextDouble();
+            double susceptibilityToInfluence = rand.nextDouble();
+            voters.add(new Voter(initialPreference, opinionStrength, susceptibilityToInfluence));
+        }
+    }
+
+    public void runSimulation() {
+
+    }
+}
