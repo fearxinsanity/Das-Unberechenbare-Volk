20297f698e3e91df33a856044eb877f0ab06d219
Nico Hoffmann <dev.nhoffmann@proton.me>
2025-09-03 20:52:22 +0200

    Launch JavaFX application from main class and update workspace configuration

--- src/com/unberechenbarevolk/Main.java
+++ src/com/unberechenbarevolk/Main.java
@@ -1,7 +1,9 @@
 package com.unberechenbarevolk;
+import com.unberechenbarevolk.view.DashboardUI;
+import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Unberechenbare Volk!");
+        Application.launch(DashboardUI.class, args);
     }
 }
