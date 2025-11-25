68afc05ccb617449983dbdc6304b94bba41d33ed
Nico Hoffmann <dev.nhoffmann@proton.me>
2025-11-25 21:30:09 +0100

    Removed everything and fresh start

--- src/main/java/de/schulprojekt/duv/model/engine/VoterTransition.java
+++ src/main/java/de/schulprojekt/duv/model/engine/VoterTransition.java
@@ -6,30 +6,4 @@ package de.schulprojekt.duv.model.engine;
  */
 public class VoterTransition {
 
-    private final String fromPartyName;
-    private final String toPartyName;
-    private final int voterCount;
-
-    /**
-     * @param fromPartyName The name of the originating party.
-     * @param toPartyName   The name of the destination party.
-     * @param voterCount    The number of voters in this specific transitioning.
-     */
-    public VoterTransition(String fromPartyName, String toPartyName, int voterCount) {
-        this.fromPartyName = fromPartyName;
-        this.toPartyName = toPartyName;
-        this.voterCount = voterCount;
-    }
-
-    public String getFromPartyName() {
-        return fromPartyName;
-    }
-
-    public String getToPartyName() {
-        return toPartyName;
-    }
-
-    public int getVoterCount() {
-        return voterCount;
-    }
 }
--- src/main/java/de/schulprojekt/duv/view/Main.java
+++ src/main/java/de/schulprojekt/duv/view/Main.java
@@ -1,15 +1,8 @@
 package de.schulprojekt.duv.view;
 
-// --- JavaFX ---
-import de.schulprojekt.duv.controller.SimulationController;
-import de.schulprojekt.duv.model.engine.SimulationEngine;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URL;
 
 /**
  * The main entry point for the JavaFX application.
@@ -19,26 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        // Load FXML file from folder
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 900); //Default Size
 
-        DashboardController dashboardController = fxmlLoader.getController();
-        SimulationEngine model = new SimulationEngine();
-        SimulationController simulationController = new SimulationController(model, dashboardController);
-        simulationController.initializeSimulation();
-
-        //Load the CSS file
-        URL cssUrl = Main.class.getResource("styles.css");
-        if (cssUrl != null) {
-            scene.getStylesheets().add(cssUrl.toExternalForm());
-        } else {
-            System.err.println("CSS resource not found");
-        }
-
-        primaryStage.setTitle("Das unberechenbare Volk");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     /**
