9b3d443ac12524c142c3bcf0e38eee406057add5
Nico Hoffmann <dev.nhoffmann@proton.me>
2025-12-08 12:14:39 +0100

    Added parameters to SimulationParameters.java

--- src/main/java/de/schulprojekt/duv/model/engine/SimulationParameters.java
+++ src/main/java/de/schulprojekt/duv/model/engine/SimulationParameters.java
@@ -3,4 +3,12 @@ package de.schulprojekt.duv.model.engine;
 
 public class SimulationParameters {
 
+    private int totalVoterCount;
+    private double globalMediaInfluence;
+    private double baseMobilityRate;
+    private double scandalChance;
+    private double initialLoyaltyMean;
+    private int simulationTicksPerSecond;
+    private double uniformRandomRange;
+
 }
