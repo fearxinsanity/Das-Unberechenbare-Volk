d716d0b62554ff433b4461b752d4bcef9ed6d4f0
Nico Hoffmann <182205856+fearxinsanity@users.noreply.github.com>
2026-01-23 23:33:35 +0100

    Update ScandalScheduler class documentation
    Replaced the English class-level Javadoc with a German version and removed the method-level Javadoc for shouldScandalOccur. Also reverted the @version tag from 1.1 to 1.0.

--- src/main/java/de/schulprojekt/duv/model/scandal/ScandalScheduler.java
+++ src/main/java/de/schulprojekt/duv/model/scandal/ScandalScheduler.java
@@ -3,9 +3,9 @@ package de.schulprojekt.duv.model.scandal;
 import de.schulprojekt.duv.model.random.DistributionProvider;
 
 /**
- * Schedules the occurrence of scandals based on probability distributions.
+ * Plant das Auftreten von Skandalen basierend auf Wahrscheinlichkeitsverteilungen.
  * @author Nico Hoffmann
- * @version 1.1
+ * @version 1.0
  */
 public class ScandalScheduler {
 
@@ -28,11 +28,7 @@ public class ScandalScheduler {
     // ========================================
     // Business Logic Methods
     // ========================================
-
-    /**
-     * Checks if a new scandal should be triggered in the current step.
-     * @return true if a scandal occurs
-     */
+    
     public boolean shouldScandalOccur() {
         timeUntilNextScandal -= 1.0;
 
